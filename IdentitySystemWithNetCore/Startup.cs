2dde3a940a4b060b018ee336c42fa6a56375d89e
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-20 18:48:52 +0300

    HomeController Jquery bootsrap kütüphanelerli

--- IdentitySystemWithNetCore/Startup.cs
+++ IdentitySystemWithNetCore/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,25 +16,28 @@ namespace IdentitySystemWithNetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddControllers(options => options.EnableEndpointRouting = false);
+
+            //services.AddMvcCore();
+            //json çıktı almak istiyorum dersen AddMvcCore olabilir
         }
 
         // This method gets called  by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            //Hata ile ilgili açıklayıcı bilgi sunar. 
+            app.UseStatusCodePages();
+            //Özellikle herhangi bir içerik dönmeyen sayfalarda bilgilendirici yazılar döner. 
+            //Boş bir sayfa görmek yerine hatanın nerde olduğunu gösteren içerik döner. 
+            app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseMvcWithDefaultRoute();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+
+            //arka planda Controller/Action/{id} route oluşturur. 
+
         }
     }
 }
