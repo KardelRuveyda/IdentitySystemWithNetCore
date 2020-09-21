aa32dfdf8cd20a15b5e6d064421950af34ea02e3
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-21 11:43:02 +0300

    dotnet ef migrations add initial dotnet ef database update models/ AppUser, AppIdentityDbContext appsettings.json

--- IdentitySystemWithNetCore/Startup.cs
+++ IdentitySystemWithNetCore/Startup.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentitySystemWithNetCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,15 +16,31 @@ namespace IdentitySystemWithNetCore
 {
     public class Startup
     {
+        public IConfiguration configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddControllers(options => options.EnableEndpointRouting = false);
-
             //services.AddMvcCore();
             //json çıktı almak istiyorum dersen AddMvcCore olabilir
+
+            services.AddDbContext<AppIdentityDbContext>(opts =>
+            {
+                opts.UseSqlServer(configuration["ConnectionStrings:DefaultConnectionString"]);
+
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
+
         }
 
         // This method gets called  by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +54,7 @@ namespace IdentitySystemWithNetCore
             app.UseStaticFiles();
 
             app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
 
 
             //arka planda Controller/Action/{id} route oluşturur. 
